bbfef378eb80175f7da883841aacf9465734c9a6
Yasser Fadl <yasser.fadl@gmail.com>
2016-06-14 19:02:39 +0200

    Updating to latest specs

--- src/connector.js
+++ src/connector.js
@@ -1,74 +1,74 @@
-var events = require( 'events' ),
-	util = require( 'util' ),
-	pckg = require( '../package.json' ),
-	mongoClient = require('mongodb').MongoClient,
-	ObjectID = require('mongodb').ObjectID;
+const events = require( 'events' )
+const  util = require( 'util' )
+const  pckg = require( '../package.json' )
+const  mongoClient = require('mongodb').MongoClient
+const  ObjectID = require('mongodb').ObjectID
 
 /**
  * Connects deepstream to MongoDb.
- * 
+ *
  * Collections, ids and performance
  * --------------------------------------------------
  * Deepstream treats its storage like a simple key value store. But there are a few things
  * we can do to speed it up when using MongoDb. Mainly: using smaller (e.g. more granular) collections and using successive Id's
  *
- * 
- * To support multiple collections pass a splitChar setting to this class. This setting specifies a character 
+ *
+ * To support multiple collections pass a splitChar setting to this class. This setting specifies a character
  * at which keys will be split and ordered into collections. This sounds a bit complicated, but all that means is the following:
- * 
+ *
  * Imagine you want to store a few users. Just specify their recordNames as e.g.
- * 
- * 	user/i4vcg5j1-16n1qrnziuog
- *	user/i4vcg5x9-a2wc3g9pbhmi
- *	user/i4vcg74u-21ufhl1qs8fh
- * 
+ *
+ *  user/i4vcg5j1-16n1qrnziuog
+ *  user/i4vcg5x9-a2wc3g9pbhmi
+ *  user/i4vcg74u-21ufhl1qs8fh
+ *
  * and in your options set
- * 
+ *
  * { splitChar: '/' }
- * 
+ *
  * This way the MongoDB connector will create a 'user' collection the first time
  * it encounters this recordName and will subsequently store users in it. This will
  * improve the speed of read operations since MongoDb has to look through a smaller
  * amount of datasets to find your record
- * 
+ *
  * On top of this, it makes sense to use successive ids. MongoDb will optimise collections
  * by putting documents with similar ids next to each other. Fortunately, the build-in getUid()
  * method of the deepstream client already produces semi-succesive ids. Notice how the first bits of the
  * ids (user/i4vcg5) are all the same. These are Base36 encoded timestamps, facilitating almost succesive ordering.
- * 
+ *
  * @param {Object} options
- * 
+ *
  * {
  *    // Optional: Collections for items without a splitChar or if no splitChar is specified. Defaults to 'deepstream_docs'
- 	 defaultCollection: <String>,
- 	 
- 	 // Optional: A char that seperates the collection name from the document id. Defaults to null
- 	 splitChar: <String>,
- 	 
- 	 // Full connection URL for MongoDb. Format is mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
- 	 // More details can be found here: http://docs.mongodb.org/manual/reference/connection-string/
- 	 connectionString: <String>
+   defaultCollection: <String>,
+
+   // Optional: A char that seperates the collection name from the document id. Defaults to null
+   splitChar: <String>,
+
+   // Full connection URL for MongoDb. Format is mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+   // More details can be found here: http://docs.mongodb.org/manual/reference/connection-string/
+   connectionString: <String>
    }
  *
  * @constructor
  */
 var Connector = function( options ) {
-	this.isReady = false;
-	this.name = pckg.name;
-	this.version = pckg.version;
-	this._splitChar = options.splitChar || null;
-	this._defaultCollection = options.defaultCollection || 'deepstream_docs';
-	this._db = null;
-	this._collections = {};
-	
-	if( !options.connectionString ) {
-		throw new Error( 'Missing setting \'connectionString\'' );
-	}
-	
-	mongoClient.connect( options.connectionString, this._onConnect.bind( this ) );
-};
+  this.isReady = false
+  this.name = pckg.name
+  this.version = pckg.version
+  this._splitChar = options.splitChar || null
+  this._defaultCollection = options.defaultCollection || 'deepstream_docs'
+  this._db = null
+  this._collections = {}
 
-util.inherits( Connector, events.EventEmitter );
+  if( !options.connectionString ) {
+    throw new Error( 'Missing setting \'connectionString\'' )
+  }
+
+  mongoClient.connect( options.connectionString, this._onConnect.bind( this ) )
+}
+
+util.inherits( Connector, events.EventEmitter )
 
 /**
  * Writes a value to the cache.
@@ -81,16 +81,16 @@ util.inherits( Connector, events.EventEmitter );
  * @returns {void}
  */
 Connector.prototype.set = function( key, value, callback ) {
-	var params = this._getParams( key );
-	
-	if( params === null ) {
-		callback( 'Invalid key ' + key );
-		return;
-	}
-	
-	value.ds_key = params.id;
-	params.collection.updateOne({ ds_key: params.id }, value, { upsert: true }, callback );
-};
+  var params = this._getParams( key )
+
+  if( params === null ) {
+    callback( 'Invalid key ' + key )
+    return
+  }
+
+  value.ds_key = params.id
+  params.collection.updateOne({ ds_key: params.id }, value, { upsert: true }, callback )
+}
 
 /**
  * Retrieves a value from the cache
@@ -103,27 +103,27 @@ Connector.prototype.set = function( key, value, callback ) {
  * @returns {void}
  */
 Connector.prototype.get = function( key, callback ) {
-	var params = this._getParams( key );
-	
-	if( params === null ) {
-		callback( 'Invalid key ' + key );
-		return;
-	}
-	
-	params.collection.findOne({ ds_key: params.id }, function( err, doc ){
-		if( err ) {
-			callback( err );
-		} else {
-			if( doc === null ) {
-				callback( null, null );
-			} else {
-				delete doc._id;
-				delete doc.ds_key;
-				callback( null, doc );
-			}
-		}
-	});
-};
+  var params = this._getParams( key )
+
+  if( params === null ) {
+    callback( 'Invalid key ' + key )
+    return
+  }
+
+  params.collection.findOne({ ds_key: params.id }, ( err, doc ) => {
+    if( err ) {
+      callback( err )
+    } else {
+      if( doc === null ) {
+        callback( null, null )
+      } else {
+        delete doc._id
+        delete doc.ds_key
+        callback( null, doc )
+      }
+    }
+  })
+}
 
 /**
  * Deletes an entry from the cache.
@@ -136,74 +136,74 @@ Connector.prototype.get = function( key, callback ) {
  * @returns {void}
  */
 Connector.prototype.delete = function( key, callback ) {
-	var params = this._getParams( key );
-	
-	if( params === null ) {
-		callback( 'Invalid key ' + key );
-		return;
-	}
-	
-	params.collection.deleteOne({ ds_key: params.id }, callback );
-};
+  var params = this._getParams( key )
+
+  if( params === null ) {
+    callback( 'Invalid key ' + key )
+    return
+  }
+
+  params.collection.deleteOne({ ds_key: params.id }, callback )
+}
 
 /**
  * Callback for established (or rejected) connections
- * 
+ *
  * @param {String} error
  * @param {MongoClient} db
- * 
+ *
  * @private
  * @returns {void}
  */
 Connector.prototype._onConnect = function( err, db ) {
-	if( err ) {
-		this.emit( 'error', err );
-		return;
-	}
-	
-	this._db = db;
-	this.isReady = true;
-	this.emit( 'ready' );
-};
+  if( err ) {
+    this.emit( 'error', err )
+    return
+  }
+
+  this._db = db
+  this.isReady = true
+  this.emit( 'ready' )
+}
 
 /**
  * Determines the document id and the collection
  * to use based on the provided key
- * 
+ *
  * Creates the collection if it doesn't exist yet.
- * 
+ *
  * Since MongoDB ObjecIDs are adhering to a specified format
  * we'll add a new field for the key called ds_key and index the
  * collection based on it
- * 
+ *
  * @param {String} key
- * 
+ *
  * @private
  * @returns {Object} {connection: <MongoConnection>, id: <String> }
  */
 Connector.prototype._getParams = function( key ) {
-	var parts = key.split( this._splitChar ),
-		collectionName,
-		id;
-	
-	if( parts.length === 1 ) {
-		collectionName = this._defaultCollection;
-		id = key;
-	}
-	else if( parts.length === 2 ) {
-		collectionName = parts[ 0 ];
-		id = parts[ 1 ];
-	}
-	else {
-		return null;
-	}
-	
-	if( !this._collections[ collectionName ] ) {
-		this._collections[ collectionName ] = this._db.collection( collectionName );
-		this._collections[ collectionName ].ensureIndex({ ds_key: 1 });
-	}
-	
-	return { collection: this._collections[ collectionName ], id: id };
-};
+  var parts = key.split( this._splitChar ),
+    collectionName,
+    id
 
-module.exports = Connector;
+  if( parts.length === 1 ) {
+    collectionName = this._defaultCollection
+    id = key
+  }
+  else if( parts.length === 2 ) {
+    collectionName = parts[ 0 ]
+    id = parts[ 1 ]
+  }
+  else {
+    return null
+  }
+
+  if( !this._collections[ collectionName ] ) {
+    this._collections[ collectionName ] = this._db.collection( collectionName )
+    this._collections[ collectionName ].ensureIndex({ ds_key: 1 })
+  }
+
+  return { collection: this._collections[ collectionName ], id: id }
+}
+
+module.exports = Connector
--- test/cache-connectorSpec.js
+++ test/cache-connectorSpec.js
@@ -1,85 +1,89 @@
 /* global describe, expect, it, jasmine */
-var CacheConnector = require( '../src/connector' ),
-	EventEmitter = require( 'events' ).EventEmitter,
-	settings = { connectionString: '<enter connection string>', splitChar: '/' },
-	MESSAGE_TIME = 20;
+const expect = require('chai').expect
+const CacheConnector = require( '../src/connector' )
+const EventEmitter = require( 'events' ).EventEmitter
+const settings = {
+  connectionString: process.env.MONGODB_CONNECTION_STRING,
+  splitChar: '/'
+}
+const MESSAGE_TIME = 20
 
-describe( 'the message connector has the correct structure', function(){
-	var cacheConnector;
-	it( 'throws an error if required connection parameters are missing', function(){
-		expect(function(){ new CacheConnector( 'gibberish' ); }).toThrow();
-	});
-	
-	it( 'creates the cacheConnector', function( done ){
-		cacheConnector = new CacheConnector( settings );
-		expect( cacheConnector.isReady ).toBe( false );
-		cacheConnector.on( 'error', function( err ){ throw err; });
-		cacheConnector.on( 'ready', done );
-	});
-	
-	it( 'implements the cache/storage connector interface', function() {
-	    expect( typeof cacheConnector.name ).toBe( 'string' );
-	    expect( typeof cacheConnector.version ).toBe( 'string' );
-	    expect( typeof cacheConnector.get ).toBe( 'function' );
-	    expect( typeof cacheConnector.set ).toBe( 'function' );
-	    expect( typeof cacheConnector.delete ).toBe( 'function' );
-	    expect( cacheConnector instanceof EventEmitter ).toBe( true );
-	});
-	
-	it( 'parses keys', function() {
-		var params = cacheConnector._getParams( 'user/a' );
-		expect( params.collection.collectionName ).toBe( 'user' );
-		expect( params.id ).toBe( 'a' );
-		
-		params = cacheConnector._getParams( 'bla' );
-		expect( params.collection.collectionName ).toBe( 'deepstream_docs' );
-		expect( params.id ).toBe( 'bla' );
-		
-		params = cacheConnector._getParams( 'a/b/c' );
-		expect( params ).toBe( null );
-	});
-	
-	it( 'refuses updates with invalid keys', function() {
-	    cacheConnector.set( '/a/b/c', {}, function( err ){
-	    	expect( err ).toBe( 'Invalid key /a/b/c' );
-	    });
-	});
-	
-	it( 'retrieves a non existing value', function( done ){
-		cacheConnector.get( 'someValue', function( error, value ){
-			expect( error ).toBe( null );
-			expect( value ).toBe( null );
-			done();
-		});
-	});
-	
-	it( 'sets a value', function( done ){
-		cacheConnector.set( 'someValue', { firstname: 'Wolfram' }, function( error ){
-			expect( error ).toBe( null );
-			done();
-		});
-	});
-	
-	it( 'retrieves an existing value', function( done ){
-		cacheConnector.get( 'someValue', function( error, value ){
-			expect( error ).toBe( null );
-			expect( value ).toEqual( { firstname: 'Wolfram' } );
-			done();
-		});
-	});
-	
-	it( 'deletes a value', function( done ){
-		cacheConnector.delete( 'someValue', function( error ){
-			expect( error ).toBe( null );
-			done();
-		});
-	});
-	
-	it( 'Can\'t retrieve a deleted value', function( done ){
-		cacheConnector.get( 'someValue', function( error, value ){
-			expect( error ).toBe( null );
-			expect( value ).toBe( null );
-			done();
-		});
-	});
-});
+describe( 'the message connector has the correct structure', () => {
+  var cacheConnector
+  it( 'throws an error if required connection parameters are missing', () => {
+    expect(() => { new CacheConnector( 'gibberish' ) }).to.throw()
+  })
+
+  it( 'creates the cacheConnector', ( done ) => {
+    cacheConnector = new CacheConnector( settings )
+    expect( cacheConnector.isReady ).to.equal( false )
+    cacheConnector.on( 'error', ( err ) => { throw err })
+    cacheConnector.on( 'ready', done )
+  })
+
+  it( 'implements the cache/storage connector interface', () => {
+    expect( typeof cacheConnector.name ).to.equal( 'string' )
+    expect( typeof cacheConnector.version ).to.equal( 'string' )
+    expect( typeof cacheConnector.get ).to.equal( 'function' )
+    expect( typeof cacheConnector.set ).to.equal( 'function' )
+    expect( typeof cacheConnector.delete ).to.equal( 'function' )
+    expect( cacheConnector instanceof EventEmitter ).to.equal( true )
+  })
+
+  it( 'parses keys', () => {
+    var params = cacheConnector._getParams( 'user/a' )
+    expect( params.collection.collectionName ).to.equal( 'user' )
+    expect( params.id ).to.equal( 'a' )
+
+    params = cacheConnector._getParams( 'bla' )
+    expect( params.collection.collectionName ).to.equal( 'deepstream_docs' )
+    expect( params.id ).to.equal( 'bla' )
+
+    params = cacheConnector._getParams( 'a/b/c' )
+    expect( params ).to.equal( null )
+  })
+
+  it( 'refuses updates with invalid keys', () => {
+    cacheConnector.set( '/a/b/c', {}, ( err ) => {
+      expect( err ).to.equal( 'Invalid key /a/b/c' )
+    })
+  })
+
+  it( 'retrieves a non existing value', ( done ) => {
+    cacheConnector.get( 'someValue', ( error, value ) => {
+      expect( error ).to.equal( null )
+      expect( value ).to.equal( null )
+      done()
+    })
+  })
+
+  it( 'sets a value', ( done ) => {
+    cacheConnector.set( 'someValue', { firstname: 'Wolfram' }, ( error ) => {
+      expect( error ).to.equal( null )
+      done()
+    })
+  })
+
+  it( 'retrieves an existing value', ( done ) => {
+    cacheConnector.get( 'someValue', ( error, value ) => {
+      expect( error ).to.equal( null )
+      expect( value ).to.deep.equal( { firstname: 'Wolfram' } )
+      done()
+    })
+  })
+
+  it( 'deletes a value', ( done ) => {
+    cacheConnector.delete( 'someValue', ( error ) => {
+      expect( error ).to.equal( null )
+      done()
+    })
+  })
+
+  it( 'Can\'t retrieve a deleted value', ( done ) => {
+    cacheConnector.get( 'someValue', ( error, value ) => {
+      expect( error ).to.equal( null )
+      expect( value ).to.equal( null )
+      done()
+    })
+  })
+})
